841745595543695c6a1aa1cce06b8e96900768a2
Omar <omaralex131191@gmail.com>
2014-06-12 13:19:49 -0500

    cambio de hora en el stopwatch.js

--- assets/js/stopwatch.js
+++ assets/js/stopwatch.js
@@ -1,7 +1,7 @@
 getTime();
 function getTime(){
     now = new Date();
-    objetiveDate = new Date("Jun 12 2014 17:00:00");
+    objetiveDate = new Date("Jun 12 2014 15:00:00");
     days = (objetiveDate - now) / 1000 / 60 / 60 / 24;
     daysRound = Math.floor(days);
     hours = (objetiveDate - now) / 1000 / 60 / 60 - (24 * daysRound);
@@ -12,10 +12,14 @@ function getTime(){
     secondsRound = Math.round(seconds);
 
     sec = (secondsRound == 1) ? " segundo" : " segundos";
-    min = (minutesRound == 1) ? " minuto" : " minutos, ";
-    hr = (hoursRound == 1) ? " hora" : " horas, ";
+    min = (minutesRound == 1) ? " minuto " : " minutos, ";
+    hr = (hoursRound == 1) ? " hora " : " horas, ";
     day = (daysRound == 1)  ? " d&iacute;a " : " d&iacute;as, ";
 
-    document.getElementById("headerstopwatch").innerHTML = "Faltan <span>" + daysRound  + day + hoursRound + hr + minutesRound + min + secondsRound + sec + "</span> <br>para el primer partido del mundial";
+    if(days < 0){
+        document.getElementById("headerstopwatch").innerHTML = "";
+    }else{
+        document.getElementById("headerstopwatch").innerHTML = "Faltan <span>" + daysRound  + day + hoursRound + hr + minutesRound + min + secondsRound + sec + "</span> <br>para el primer partido del mundial";
+    }
     newtime = window.setTimeout("getTime();", 1000);
 }
